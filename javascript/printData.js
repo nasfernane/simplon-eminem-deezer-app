17098c52385005f59079446471fefa6a709644ed
nasfernane <nasfernane@gmail.com>
2020-12-03 18:04:52 +0100

    bug fix

--- javascript/printData.js
+++ javascript/printData.js
@@ -7,7 +7,7 @@ const albumsDiv = document.querySelector('.album-list');
 const modeleCard = document.querySelector('.album-list .card');
 
 // crée un compteur pour la position de l'album
-let albumPosition = 0;
+let albumPosition = 1;
 
 const displayAlbums = function () {
     // pour chaque album
@@ -17,17 +17,22 @@ const displayAlbums = function () {
         // On insère la card sur la page HTML
         albumsDiv.insertAdjacentElement('beforeend', tempDiv);
         // on implémente les données de l'album dans la div card
-        document.querySelector(`.card:last-child img`).src =
+        document.querySelector(`.card:nth-child(${albumPosition}) img`).src =
             item.album.cover_big;
-        document.querySelector(`.card:last-child img`).alt =
+        document.querySelector(`.card:nth-child(${albumPosition}) img`).alt =
             item.album.cover_big;
-        document.querySelector(`.card:last-child .card-body a`).href =
-            item.album.tracklist;
-        document.querySelector(`.card:last-child .card-body h5`).textContent =
-            item.title;
-        document.querySelector(`.card:last-child .card-body audio`).src =
-            item.preview;
-        document.querySelector(`.card:last-child`).style.display = 'block';
+        document.querySelector(
+            `.card:nth-child(${albumPosition}) .card-body a`
+        ).href = item.album.tracklist;
+        document.querySelector(
+            `.card:nth-child(${albumPosition}) .card-body h5`
+        ).textContent = item.title;
+        document.querySelector(
+            `.card:nth-child(${albumPosition}) .card-body audio`
+        ).src = item.preview;
+        document.querySelector(
+            `.card:nth-child(${albumPosition})`
+        ).style.display = 'block';
 
         // on incrémente le compteur de position
         albumPosition++;
